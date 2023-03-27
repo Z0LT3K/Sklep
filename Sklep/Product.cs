1dd4316d8b954dda9e81e324dd3f04aba3182c0a
Z0LT3K <bartolini18j@gmail.com>
2023-03-28 00:18:29 +0200

    Add + wyswietlanie Dziala

--- Sklep/Product.cs
+++ Sklep/Product.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Sklep
 {
@@ -8,9 +10,14 @@ namespace Sklep
     {
         public string Name { get; set; }
         public int Quantity { get; set; }
-        public Type Type{ get; set; }
+        public Type Type { get; set; }
         public decimal Price { get; set; }
         public Count Count { get; set; }
-        
+        public override string ToString()
+        {
+            return $"{Name, -10} {Quantity, -5} {Type, -10} {Price, -6} {Count, -7}";
+
+        }
+
     }
 }
--- Sklep/Program.cs
+++ Sklep/Program.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using CsvHelper;
 using CsvHelper.Configuration;
-using CsvHelper.Configuration.Attributes;
+using LINQtoCSV;
+using Sklep;
 
 namespace Sklep
 {
@@ -15,39 +16,61 @@ namespace Sklep
     {
         static void Main(string[] args)
         {
-            string csvPath = @"C:\Users\Bartosz\source\repos\Sklep\Sklep\Pruducts.csv";
-
+            string csvPath = @"C:\Users\Bartosz\source\repos\Sklep\Sklep\Products.csv";
             var allProducts = LoadProducts(csvPath);
 
-            ShowAll(allProducts);
+
+            Methots.AddProduct();
+           
+
+            Console.WriteLine("Naciśnij strzałkę w górę lub w dół, aby poruszać się po liście. Naciśnij Enter, aby wyjść.");
+
+            Console.ReadKey();
+            Display(allProducts);
+
+        }
+       
+        public static void GetTypes()
+        {
+            foreach (int i in Enum.GetValues(typeof(Type)))
+                Console.WriteLine(i);
+
+        }
+        #region Display
+        static void GetData(IEnumerable<Product> allProducts)
+        {
+            var BreadProduct = allProducts.Where(a => a.Type == Type.Bread);
+            Display(BreadProduct);
         }
 
-
-        static void Display(Product product)
+        static void Display(IEnumerable<Product> allProducts)
         {
-            Console.WriteLine(product);
-        }
-
-        static void ShowAll(IEnumerable<Product> products)
-        {
-            foreach(var product in products)
+            foreach (var product in allProducts)
             {
                 Console.WriteLine(product);
             }
         }
-        static List<Product> LoadProducts(string csvPath)
+
+        #endregion
+
+        #region CsvReader 
+        public static List<Product> LoadProducts(string csvPath)
         {
             using (var reader = new StreamReader(csvPath))
             using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
             {
-                
+
                 csv.Context.RegisterClassMap<ProductsMap>();
                 var records = csv.GetRecords<Product>().ToList();
-                
+
                 return records;
             }
 
         }
 
+        
+        #endregion
+
     }
+
 }
